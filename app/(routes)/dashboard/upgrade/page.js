546fcbe4eec1932404e357c5287f0d66fb679e5f
Alain Descartes <ualaindescartes@gmail.com>
2024-08-09 02:38:30 -0600

    added an upgrade page route

---
+++ app/(routes)/dashboard/upgrade/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="flex items-center  mt-40 border border-gray-200 rounded-lg h-[100px] shadow-lg m-5 animate-bounce">
+      <h1 className="font-bold text-primary text-3xl w-full text-center ">
+        Coming Soon.....
+      </h1>
+    </div>
+  );
+}
